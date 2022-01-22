ba2870bf3c379d9b64c346b836bf669fd935c5f1
Miguel-A-Jara <0miguel.jara15@gmail.com>
2022-01-22 11:42:16 -0300

    "Finished style configuration and added functionality"

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 //Defining the "End-Point"
 const pokemonURL = 'https://pokeapi.co/api/v2/';
+const pokemonGender = 'https://pokeapi.co/api/v2/gender/';
 const pokemonContainer = document.querySelector('#pokemonContainer');
 const searchBar = document.querySelector('#pokeName');
 
@@ -18,13 +19,6 @@ function listenToElements(type, callback){
 
 
 
-//We listen to the form button
-listenToElements('#searchButton', () => {
-    makeApiRequest(searchBar.value.toLowerCase());
-})
-
-
-
 
 //We make the API request
 async function makeApiRequest(pathName){
@@ -35,14 +29,11 @@ async function makeApiRequest(pathName){
             .then(response => response.json())
             .then(data => {return data});
 
-            printPokemon(fetchResponse); //Calling the HTML "printing" method
+            printPokemon(fetchResponse);
 
         } catch (error) {
             pokemonContainer.innerHTML = 'Introduce a correct Pokémon name please!';
         }
-
-
-        
 
     } else {
         pokemonContainer.innerHTML = 'Input some data, please!';
@@ -51,9 +42,36 @@ async function makeApiRequest(pathName){
 
 
 
+function spinnerLoadAnimation(){
+    pokemonContainer.innerHTML = `<div id="spinnerDiv"></div>`;
+}
+
+
+
 
 function printPokemon(pokemonData){
     pokemonContainer.innerHTML = `
-        <img src="${pokemonData.sprites.other.dream_world.front_default}">
+        <div id="pokemonCard">
+            <section>
+                <img src="${pokemonData.sprites.front_default}" alt="Pokemon Image not found">
+                <img src="${pokemonData.sprites.back_default}" alt="Pokemon Image not found">
+            </section>
+            <section>
+                <h2>${pokemonData.name.toUpperCase()}</h2>
+                <span>Weight: ${pokemonData.weight}</span>
+                <span>Height: ${pokemonData.height}</span>
+                <span>ID: ${pokemonData.id}</span>
+                <span>Base Experience: ${pokemonData.base_experience}</span>
+                </section>
+        </div>
     `;
 }
+
+
+
+
+//We listen to the form button
+listenToElements('#searchButton', () => {
+    spinnerLoadAnimation();
+    makeApiRequest(searchBar.value.toLowerCase());
+})
